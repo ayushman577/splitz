ab35fa1e416a69e8e70c1c4ac2dbb118b250ed08
ayushman577 <ayushmannanda577@gmail.com>
2026-09-10 18:32:53 +0530

    google login fixed

--- src/auth.ts
+++ src/auth.ts
@@ -14,7 +14,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   },
 
   providers: [
-    Google,
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
 
     Credentials({
       credentials: {
